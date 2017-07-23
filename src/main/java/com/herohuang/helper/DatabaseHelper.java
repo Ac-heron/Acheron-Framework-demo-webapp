170c6d0001261de5d6e44f8e483c885d83ef364c
Ac-heron <acheron20122012@gmail.com>
2017-07-23 10:27:54 +0800

    将connetion放入ThreadLocal中

--- src/main/java/com/herohuang/helper/DatabaseHelper.java
+++ src/main/java/com/herohuang/helper/DatabaseHelper.java
@@ -2,6 +2,7 @@ package com.herohuang.helper;
 
 import com.herohuang.util.PropsUtil;
 import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanHandler;
 import org.apache.commons.dbutils.handlers.BeanListHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,7 +17,15 @@ public final class DatabaseHelper {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(DatabaseHelper.class);
 
+    /**
+     * Apache Common DbUtils对jdbc的封装
+     */
     private static final QueryRunner QUERY_RUNNER = new QueryRunner();
+
+    /**
+     * 确保一个线程中只有一个connection
+     */
+    private static final ThreadLocal<Connection> CONNECTION_THREAD_LOCAL = new ThreadLocal<>();
 
     private static final String DRIVER;
     private static final String URL;
@@ -37,35 +46,77 @@ public final class DatabaseHelper {
     }
 
     public static Connection getConnection() {
-        Connection conn = null;
-        try {
-            conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-        } catch (SQLException e) {
-            LOGGER.error("Get connection failure", e);
+        Connection conn = CONNECTION_THREAD_LOCAL.get();
+        if (null == conn) {
+            try {
+                conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            } catch (SQLException e) {
+                LOGGER.error("Get connection failure", e);
+                throw new RuntimeException(e);
+            } finally {
+                CONNECTION_THREAD_LOCAL.set(conn);
+            }
         }
         return conn;
     }
 
-    public static void closeConncetion(Connection conn) {
+    public static void closeConncetion() {
+        Connection conn = CONNECTION_THREAD_LOCAL.get();
         if (conn != null) {
             try {
                 conn.close();
             } catch (SQLException e) {
                 LOGGER.error("Close connection failure", e);
+                throw new RuntimeException(e);
+            } finally {
+                CONNECTION_THREAD_LOCAL.remove();
             }
         }
     }
 
-    public static <T> List<T> queryEntityList(Class<T> entityClass,Connection conn, String sql, Object... params) {
+    /**
+     * 查询集合
+     *
+     * @param entityClass
+     * @param sql
+     * @param params
+     * @param <T>
+     * @return
+     */
+    public static <T> List<T> queryEntityList(Class<T> entityClass, String sql, Object... params) {
         List<T> entityList;
         try {
+            Connection conn = getConnection();
             entityList = QUERY_RUNNER.query(conn, sql, new BeanListHandler<T>(entityClass), params);
         } catch (SQLException e) {
             LOGGER.error("Query entity list failure", e);
             throw new RuntimeException(e);
         } finally {
-            closeConncetion(conn);
+            closeConncetion();
         }
         return entityList;
+    }
+
+    /**
+     * 查询单个实体
+     *
+     * @param entityClass
+     * @param sql
+     * @param params
+     * @param <T>
+     * @return
+     */
+    public static <T> T queryEntity(Class<T> entityClass, String sql, Object... params) {
+        T entity;
+        try {
+            Connection conn = getConnection();
+            entity = QUERY_RUNNER.query(conn, sql, new BeanHandler<T>(entityClass), params);
+        } catch (SQLException e) {
+            LOGGER.error("Query entity failure", e);
+            throw new RuntimeException(e);
+        } finally {
+            closeConncetion();
+        }
+        return entity;
     }
 }
--- src/main/java/com/herohuang/service/CustomerService.java
+++ src/main/java/com/herohuang/service/CustomerService.java
@@ -5,7 +5,6 @@ import com.herohuang.model.Customer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.sql.Connection;
 import java.util.List;
 
 public class CustomerService {
@@ -13,14 +12,8 @@ public class CustomerService {
     private static final Logger LOGGER = LoggerFactory.getLogger(CustomerService.class);
 
 
-
     public List<Customer> getCustomerList() {
-        Connection conn = DatabaseHelper.getConnection();
-        try {
-            String sql = "select * from customer";
-            return DatabaseHelper.queryEntityList(Customer.class, conn, sql);
-        } finally {
-            DatabaseHelper.closeConncetion(conn);
-        }
+        String sql = "select * from customer";
+        return DatabaseHelper.queryEntityList(Customer.class, sql);
     }
 }
