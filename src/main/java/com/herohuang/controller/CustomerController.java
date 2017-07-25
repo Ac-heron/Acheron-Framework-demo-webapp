ab1483e99b47dbdf3d6e48212d19ee9fc157d680
Ac-heron <acheron20122012@gmail.com>
2017-07-25 22:14:36 +0800

    使用acheron-framework

--- src/main/java/com/herohuang/controller/CustomerController.java
+++ src/main/java/com/herohuang/controller/CustomerController.java
@@ -3,6 +3,7 @@ package com.herohuang.controller;
 import com.herohuang.framework.annotation.Action;
 import com.herohuang.framework.annotation.Controller;
 import com.herohuang.framework.annotation.Inject;
+import com.herohuang.framework.bean.Data;
 import com.herohuang.framework.bean.Param;
 import com.herohuang.framework.bean.View;
 import com.herohuang.model.Customer;
@@ -27,5 +28,11 @@ public class CustomerController {
     public View index(Param param) {
         List<Customer> customerList = customerService.getCustomerList();
         return new View("customer_list.jsp").addModel("customerList", customerList);
+    }
+
+    @Action("get:/ajax")
+    public Data ajax(Param param) {
+        List<Customer> customerList = customerService.getCustomerList();
+        return new Data(customerList);
     }
 }
